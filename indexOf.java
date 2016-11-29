a5dfee70a8447a40ca396d47287d92809cec961d
Elissa <e-digitalis@users.noreply.github.com>
2016-11-29 20:05:16 +0100

    Create indexOf.java

---
+++ indexOf.java
@@ -0,0 +1,20 @@
+kod
+package testingOtherStuff;
+import java.util.Scanner;
+public class NextIndexOfString {
+	public static void main(String[] args) {
+		Scanner myScanner = new Scanner(System.in);
+		
+		String numberString = "5,10,15,25,100,1000";
+		
+		String nextCase = ",";
+		int indexBeginning = 0;
+		int indexEnd = 0;
+		for (indexBeginning = numberString.indexOf(nextCase); indexBeginning >= 0; indexBeginning = numberString.indexOf(nextCase, indexBeginning + 1)) {
+			indexEnd = numberString.indexOf(nextCase, indexBeginning + 1);
+			System.out.println("IB: " + indexBeginning);
+			System.out.println("IE: " + indexEnd);
+			myScanner.close();
+		}
+	}
+}
